326fb0ea18635df49cf9ccd5b17b3e5a6e110575
Jojo <jaime25112002@gmail.com>
2019-10-30 12:48:46 -0400

    Se agregan otros archivos y se modifica db -prueba

---
+++ ConsoleAppEntityUsuarios/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleAppEntityUsuarios
+{
+    class ApplicationDbContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=C:\\Users\\jaime\\source\\repos\\ConsoleAppEntityUsuarios\\ConsoleAppEntityUsuarios\\TodoEmpresa.db");
+
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ ConsoleAppEntityUsuarios/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleAppEntityUsuarios
+{
+    class Program
+    {
+        static String NombreUsuario="";
+        static String id;
+        static String ContraseñaUsuario = "";
+        static String seleccioncontra;
+        static int TipoUsuario;
+        static Usuario SeleccionUsu;
+        static int respu = 1;
+        static void Main(string[] args)
+        {
+            while (respu == 1)
+            {
+                Console.Clear();
+                Console.WriteLine("Ingresa tu usuario:\n\nID: ");
+                id = Console.ReadLine();
+                Console.WriteLine("\nContraseña: ");
+                seleccioncontra = Console.ReadLine();
+                try
+                {
+                    Selecciona(Convert.ToInt32(id));
+                }
+                catch(Exception ex)
+                {
+
+                }
+
+                if (ContraseñaUsuario == seleccioncontra)
+                {
+                    Console.WriteLine("\nB I E N V E N I D O ! ! !\n");
+                    Console.ReadKey();
+                    Environment.Exit(1);
+                }
+                else
+                {
+                    Console.WriteLine("\nError: ID o Contraseña incorrecta");
+                    Console.WriteLine("Desea reintentar o salir?\n1.Reintentar\n2.Salir\n\nDigite su respuesta: ");
+                    try
+                    {
+                        respu = Convert.ToInt32(Console.ReadLine());
+                    }
+                    catch(Exception e)
+                    {
+
+                    }
+                    
+
+                }
+
+
+
+            }
+
+
+
+        }
+
+        static void Selecciona(int IDusuario)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                SeleccionUsu = context.Usuarios.Where(x => x.ID == IDusuario).FirstOrDefault();
+                
+            }
+            try { 
+            NombreUsuario = SeleccionUsu.Nombre;
+            ContraseñaUsuario = SeleccionUsu.Contraseña;
+            TipoUsuario = SeleccionUsu.tipo;                
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        static void Eliminar(Usuario Buscado)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                context.Entry(Buscado).State = Microsoft.EntityFrameworkCore.EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+        
+        static void Actualizar(Usuario Buscado)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                context.Entry(Buscado).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                context.SaveChanges();
+
+            }
+        }
+
+        static void Insertar()
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var usuario = new Usuario();
+                usuario.Nombre = NombreUsuario;
+                usuario.Contraseña = ContraseñaUsuario;
+                usuario.tipo = TipoUsuario;
+                context.Usuarios.Add(usuario);
+                context.SaveChanges();
+            }
+            Console.WriteLine("Listo");
+        }
+
+    }
+
+
+
+
+    class Usuario
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Contraseña { get; set; }
+        public int tipo { get; set; }
+
+    }
+}
